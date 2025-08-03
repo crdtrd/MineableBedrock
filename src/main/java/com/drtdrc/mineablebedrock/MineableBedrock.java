5aa486d1c0962a389012fe2192aad659bac65504
Christian Deardorff <35127565+crdtrd@users.noreply.github.com>
2025-08-03 15:50:47 -0700

    Initial commit

---
+++ src/main/java/com/drtdrc/mineablebedrock/MineableBedrock.java
@@ -0,0 +1,10 @@
+package com.drtdrc.mineablebedrock;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MineableBedrock implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/drtdrc/mineablebedrock/MineableBedrockDataGenerator.java
@@ -0,0 +1,13 @@
+package com.drtdrc.mineablebedrock;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MineableBedrockDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(BedrockLootTableProvider::new);
+    }
+}
